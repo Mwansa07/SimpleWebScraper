2163040a92f8409933744755dfc5c0f0903cc1f6
Mwansa Pembanyali <mwansap07@gmail.com>
2021-01-25 01:45:18 +0200

    feat: created ScrapeCriteriaBuilder to build a ScrapeCriteria object

--- SimpleWebScraper/Builders/ScrapeCriteriaBuilder.cs
+++ SimpleWebScraper/Builders/ScrapeCriteriaBuilder.cs
@@ -19,7 +19,44 @@ namespace SimpleWebScraper.Builders
 
         private void SetDefault()
         {
-            throw new NotImplementedException();
+            _data = string.Empty;
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+            _parts = new List<ScrapeCriteriaPart>();
+        }
+
+        public ScrapeCriteriaBuilder WithData(string data)
+        {
+            _data = data;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegexOption(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithPart(ScrapeCriteriaPart part)
+        {
+            _parts.Add(part);
+            return this;
+        }
+
+        public ScrapeCriteria Build()
+        {
+            ScrapeCriteria scrapeCriteria = new ScrapeCriteria();
+            scrapeCriteria.Data = _data;
+            scrapeCriteria.Regex = _regex;
+            scrapeCriteria.RegexOption = _regexOption;
+            scrapeCriteria.Parts = _parts;
+            return scrapeCriteria;
         }
     }
 }
