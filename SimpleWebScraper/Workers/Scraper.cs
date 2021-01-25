00f5018b6bb820469ea5b0a593b8b307458040cd
Mwansa Pembanyali <mwansap07@gmail.com>
2021-01-26 01:51:05 +0200

    feat: add class to scrape the html elements

---
+++ SimpleWebScraper/Workers/Scraper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using SimpleWebScraper.Data;
+
+namespace SimpleWebScraper.Workers
+{
+    public class Scraper
+    {
+        public List<string> Scrape(ScrapeCriteria scrapeCriteria)
+        {
+            List<string> scrapedElements = new List<string>();
+
+            MatchCollection matches = Regex.Matches(scrapeCriteria.Data, scrapeCriteria.Regex, scrapeCriteria.RegexOption);
+
+            foreach (Match match in matches)
+            {
+                if (!scrapeCriteria.Parts.Any())
+                {
+                    scrapedElements.Add(match.Groups[0].Value);
+                }
+            }
+
+
+            return scrapedElements;
+        }
+    }
+}
