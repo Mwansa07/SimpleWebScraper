1733df2e964773bf411e2a4686036b68cad2181b
Mwansa Pembanyali <mwansap07@gmail.com>
2021-01-26 23:17:31 +0200

    feat: get city and category from user

--- SimpleWebScraper/Program.cs
+++ SimpleWebScraper/Program.cs
@@ -6,7 +6,11 @@ namespace SimpleWebScraper
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter which city you would like to scrape information from:");
+            var craigsListCity = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("Please enter the CraigsList category that you would like to scrape:");
+            var craigsListCategoryName = Console.ReadLine() ?? string.Empty;
         }
     }
 }
--- SimpleWebScraper/Workers/Scraper.cs
+++ SimpleWebScraper/Workers/Scraper.cs
@@ -34,7 +34,6 @@ namespace SimpleWebScraper.Workers
                 }
             }
 
-
             return scrapedElements;
         }
     }
